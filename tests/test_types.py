1a59f1df72105357a78c918cf22b8e64cc69e3eb
Mat Wall <mat.wall@digital.cabinet-office.gov.uk>
2014-08-06 10:07:06 +0100

    Added test for simple address with correct format

--- tests/test_types.py
+++ tests/test_types.py
@@ -6,11 +6,21 @@ from datatypes.core import SchemaInvalidException
 
 class TestTypes(unittest.TestCase):
     def test_address_with_no_line_one_fails_validation(self):
-        address_without_postcode = Address(
-            {
-                'city': 'sometown',
-                'postcode': 'AB123VC'
-            }
-        )
+        address_without_postcode = Address({
+            'city': 'sometown',
+            'postcode': 'AB123VC'
+        })
 
         self.assertRaises(SchemaInvalidException, address_without_postcode.validate)
+
+    def test_can_create_address_with_required_manadory_fields(self):
+        address_with_mandatory_fields = Address({
+            'line_one': '1 Accacia Avenue',
+            'city': 'Somewhereville',
+            'postcode': 'AB1235C'
+        })
+
+        try:
+            address_with_mandatory_fields.validate
+        except SchemaInvalidException:
+            self.fail('Could not validate address: ' + repr(SchemaInvalidException))
