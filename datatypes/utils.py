7a27c6d098234b2a9dd638255c9e6dcacecff2f9
Mat Wall <mat.wall@digital.cabinet-office.gov.uk>
2014-08-20 11:29:16 +0100

    Cleaned up some dead code

--- datatypes/utils.py
+++ datatypes/utils.py
@@ -11,19 +11,6 @@ def filter_none_from_dictionary(dictionary):
     return filtered
 
 
-def alphabetically_sorted_dict(d):
-    """
-    Returns a dictionary with all keys recursively sorted alphabetically
-    """
-    ordered = OrderedDict()
-    for k, v in sorted(d.items()):
-        if isinstance(v, dict):
-            ordered[k] = alphabetically_sorted_dict(v)
-        else:
-            ordered[k] = v
-    return ordered
-
-
 def translate_voluptous_errors(voluptuous_exception):
     return {e.path[0]: translate_error(e.error_message)
             for e in voluptuous_exception.errors
