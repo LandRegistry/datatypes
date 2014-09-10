dfd10d7c9f8afc4f13088f2e53cfafcfcdd51301
psd <psd@whatfettle.com>
2014-09-10 18:19:26 +0100

    Tests invalid price_paid in a title

--- tests/test_title_validation.py
+++ tests/test_title_validation.py
@@ -1,12 +1,25 @@
+# -*- coding: utf-8 -*-
 import unittest
+from copy import deepcopy
 
 from datatypes.exceptions import DataDoesNotMatchSchemaException
 
 from datatypes import title_validator
 
+simple_title = {
+    'payment': {
+        'price_paid': '3100.00'
+    }
+}
+
 class TestTitleValidation(unittest.TestCase):
-    def test_can_validate_title(self):
+    def test_can_validate_valid_title(self):
         try:
-            title_validator.validate({})
+            title_validator.validate(simple_title)
         except DataDoesNotMatchSchemaException as e:
             self.fail("Could not validate title: " + repr(e))
+
+    def test_cannot_validate_invalid_title_price_paid(self):
+        bad_title = deepcopy(simple_title)
+        bad_title['payment']['price_paid'] = '£3100.00'
+        self.assertRaises(DataDoesNotMatchSchemaException, title_validator.validate, bad_title)
