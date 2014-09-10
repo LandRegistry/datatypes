bff2aff9090288530ab35d619a707f0fe0b3b480
psd <psd@whatfettle.com>
2014-09-10 11:43:42 +0100

    Added title validator

--- datatypes/validators/title_validator.py
+++ datatypes/validators/title_validator.py
@@ -1,35 +1,34 @@
 from voluptuous import All, In
 from datatypes.core import DictionaryValidator
-from datatypes import address_validator, price_validator, geo_json_validator
+from datatypes.validators import address_validator, price_validator, geo_json_validator
 
 title_schema = {
     'title_number': All(str),
 
     'proprietors': [
         {
-            'first_name': str,
-            'last_name': str
+            'full_name': str
         }
     ],
 
     'property': {
-        'address': address_validator.schema,
+        'address': address_validator.address_schema,
         'tenure': In(['freehold', 'leasehold']),
         'class-of-title': In(['absolute', 'good', 'qualified', 'possesory'])
     },
 
     'payment': {
-        'price_paid': price_validator.schema,
+        'price_paid': price_validator.price_schema,
         'titles': [
             str  # TODO: Check that the current title number is present here?
         ]
     },
 
-    'extent': geo_json_validator.schema
+    'extent': geo_json_validator.geo_json_schema
 }
 
 
-class TitleValidator(DictionaryValidator):
+class Title(DictionaryValidator):
     def define_schema(self):
         return title_schema
 
