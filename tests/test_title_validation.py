c0b19bda72785be6088ef3b3502d56f075e78eec
Adam Shimali <adam.shimali@gmail.com>
2014-09-25 14:14:55 +0100

    Added basic validation into title for new top level keys in json.
    It seems like we should be able to do some further validation on the some if the fields of some entries, but
    I'm holding off on that until I get some more details on how consistent the data is.

--- tests/test_title_validation.py
+++ tests/test_title_validation.py
@@ -6,11 +6,29 @@ from datatypes.exceptions import DataDoesNotMatchSchemaException
 
 from datatypes import title_validator
 
+dumb_entry = {
+    "text" : "some text",
+    "fields" : {
+            "field_name_1": "something",
+            "field_name_2": {"some": "other thing"}
+    },
+    "deeds" : [],
+    "notes" : []
+}
+
 simple_title = {
-    'title_number': 'TEST123456789',
-    'payment': {
-        'price_paid': '3100.00'
-    }
+    "title_number": "TEST123456789",
+    "payment": {
+        "price_paid": "3100.00"
+    },
+    "proprietorship": dumb_entry,
+    "property_description": dumb_entry,
+    "price_paid": dumb_entry,
+    "provisions": [],
+    "easements":[],
+    "restrictive_covenants" : [],
+    "restrictions" : [],
+    "bankruptcy" : []
 }
 
 class TestTitleValidation(unittest.TestCase):
@@ -22,10 +40,53 @@ class TestTitleValidation(unittest.TestCase):
 
     def test_cannot_validate_invalid_title_number(self):
         bad_title = deepcopy(simple_title)
-        del bad_title['title_number']
-        self.assertRaisesRegexp(DataDoesNotMatchSchemaException, 'title_number is a required field', title_validator.validate, bad_title)
+        del bad_title["title_number"]
+        self.assertRaisesRegexp(DataDoesNotMatchSchemaException, "title_number is a required field", title_validator.validate, bad_title)
 
     def test_cannot_validate_invalid_title_price_paid(self):
         bad_title = deepcopy(simple_title)
-        bad_title['payment']['price_paid'] = '£3100.00'
+        bad_title["payment"]["price_paid"] = "£3100.00"
         self.assertRaises(DataDoesNotMatchSchemaException, title_validator.validate, bad_title)
+
+    def test_cannot_validate_title_without_proprietorship(self):
+        bad_title = deepcopy(simple_title)
+        del bad_title["proprietorship"]
+        self.assertRaisesRegexp(DataDoesNotMatchSchemaException, "proprietorship is a required field", title_validator.validate, bad_title)
+
+    def test_cannot_validate_title_without_property_description(self):
+        bad_title = deepcopy(simple_title)
+        del bad_title["property_description"]
+        self.assertRaisesRegexp(DataDoesNotMatchSchemaException, "property_description is a required field", title_validator.validate, bad_title)
+
+    def test_cannot_validate_title_without_price_paid(self):
+        bad_title = deepcopy(simple_title)
+        del bad_title["price_paid"]
+        self.assertRaisesRegexp(DataDoesNotMatchSchemaException, "price_paid is a required field", title_validator.validate, bad_title)
+
+    def test_cannot_validate_title_without_provisions(self):
+        bad_title = deepcopy(simple_title)
+        del bad_title["provisions"]
+        self.assertRaisesRegexp(DataDoesNotMatchSchemaException, "provisions is a required field", title_validator.validate, bad_title)
+
+    def test_cannot_validate_title_without_easements(self):
+        bad_title = deepcopy(simple_title)
+        del bad_title["easements"]
+        self.assertRaisesRegexp(DataDoesNotMatchSchemaException, "easements is a required field", title_validator.validate, bad_title)
+
+    def test_cannot_validate_title_without_restrictive_covenants(self):
+        bad_title = deepcopy(simple_title)
+        del bad_title["restrictive_covenants"]
+        self.assertRaisesRegexp(DataDoesNotMatchSchemaException, "restrictive_covenants is a required field", title_validator.validate, bad_title)
+
+    def test_cannot_validate_title_without_restrictions(self):
+        bad_title = deepcopy(simple_title)
+        del bad_title["restrictions"]
+        self.assertRaisesRegexp(DataDoesNotMatchSchemaException, "restrictions is a required field", title_validator.validate, bad_title)
+
+    def test_cannot_validate_title_without_bankruptcy(self):
+        bad_title = deepcopy(simple_title)
+        del bad_title["bankruptcy"]
+        self.assertRaisesRegexp(DataDoesNotMatchSchemaException, "bankruptcy is a required field", title_validator.validate, bad_title)
+
+
+
