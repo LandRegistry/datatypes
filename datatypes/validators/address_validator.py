da8957fd0ff66efdd4f82f15b639627f829d9758
Adam Shimali <adam.shimali@gmail.com>
2014-10-04 10:34:49 +0100

    updated address validator and tests

--- datatypes/validators/address_validator.py
+++ datatypes/validators/address_validator.py
@@ -1,17 +1,20 @@
-from voluptuous import Required, Optional, All, Length, Coerce
+from voluptuous import Required, Optional, All, Length, Coerce, Match
 from datatypes.core import DictionaryValidator
 from datatypes.validators import postcode_validator, iso_country_code_validator
 
-address_schema = {
-    Required('line_one'): All(Coerce(unicode), Length(max=40)),
-    Optional('line_two'): All(unicode, Length(max=40)),
-    Optional('line_three'): All(unicode, Length(max=40)),
-    Optional('line_four'): All(unicode, Length(max=40)),
-    Required('city'): All(unicode, Length(max=40)),
-    Required('postcode'): postcode_validator.postcode_schema,
-    Required('country'): iso_country_code_validator.country_schema
-}
+def Empty():
+    return lambda value: value.strip().isspace()
 
+address_schema = {
+    Required("full_address"): All(unicode, Empty()),
+    Required("house_no"): All(Coerce(unicode)),
+    Required("street_name"): All(unicode),
+    Required("town"): All(unicode),
+    Required("postal_county"): All(unicode),
+    Required("region_name"): All(unicode),
+    Required("postcode"): postcode_validator.postcode_schema,
+    Required("country"): All(unicode)
+}
 
 class Address(DictionaryValidator):
     def define_schema(self):
@@ -19,11 +22,12 @@ class Address(DictionaryValidator):
 
     def define_error_dictionary(self):
         return {
-            'line_one': 'line_one is a required unicode field and must be a maximum of 40 characters long',
-            'line_two': 'line_two must be a unicode which is a maximum of 40 characters long',
-            'line_three': 'line_three must be a unicode which is a maximum of 40 characters long',
-            'line_four': 'line_four must be a unicode which is a maximum of 40 characters long',
-            'city': 'city is a required unicode field and must be a maximum of 40 characters long',
-            'postcode': 'postcode must be a valid UK postcode',
-            'country': 'country must be a valid ISO 2 letter country code'
+            "full_address": "full_address is a required unicode field and must not be empty",
+            "house_no": "house_no is a required unicode field",
+            "street_name": "street_name is a required unicode field",
+            "town": "town is a required unicode field",
+            "postal_county": "postal_county is a required unicode field",
+            "region_name": "region_name is a required unicode field",
+            "postcode_schema": "postcode must be a valid UK postcode",
+            "country": "country is a required unicode field"
         }
--- tests/test_address_validation.py
+++ tests/test_address_validation.py
@@ -1,44 +1,60 @@
 import unittest
 
+from copy import deepcopy
 from datatypes import address_validator
 from datatypes.core import DataDoesNotMatchSchemaException
 
 
 class TestAddressValidation(unittest.TestCase):
-    def test_address_with_no_line_one_fails_validation(self):
-        address_without_postcode = {
-            u'city': u'sometown',
-            u'postcode': u'AB123VC'
+
+    def setUp(self):
+
+        self.address_with_mandatory_fields = {
+            u"full_address" : u"8 Acacia Avenue, Bootata, AL35PU",
+            u"house_no" : u"8",
+            u"street_name" : u"Acacia Avenue",
+            u"town" : u"Bootata",
+            u"postal_county" : u"",
+            u"region_name" : u"Smotania",
+            u"postcode" : u"AL35PU",
+            u"country" : u"Wales"
         }
 
-        self.assertRaises(DataDoesNotMatchSchemaException, address_validator.validate, address_without_postcode)
+        self.no_full_address = {
+            u"house_no" : u"8",
+            u"street_name" : u"Acacia Avenue",
+            u"town" : u"Bootata",
+            u"postal_county" : u"",
+            u"region_name" : u"Smotania",
+            u"postcode" : u"AL35PU",
+            u"country": u"Wales"
+        }
+
+        self.without_all_additional_fields = {
+            u"full_address" : u"8 Acacia Avenue, Bootata, AL35PU"
+        }
+
+
+    def test_address_with_no_full_address_fails_validation(self):
+        self.assertRaises(DataDoesNotMatchSchemaException, address_validator.validate, self.no_full_address)
+
+    def test_address_without_additional_fields_fails_validation(self):
+        self.assertRaises(DataDoesNotMatchSchemaException, address_validator.validate, self.without_all_additional_fields)
+
+    def test_address_with_whitespace_full_address_fails_validation(self):
+        address_with_whitespace_full_address = deepcopy(self.address_with_mandatory_fields )
+        address_with_whitespace_full_address["full_address"] = "    \t \n  "
+        self.assertRaises(DataDoesNotMatchSchemaException, address_validator.validate, address_with_whitespace_full_address)
 
     def test_can_create_address_with_required_mandatory_fields(self):
-        address_with_mandatory_fields = {
-            u'line_one': u'1 Acacia Avenue',
-            u'city': u'Somewhere',
-            u'postcode': u'AL35PU',
-            u'country': u'GB'
-        }
-
         try:
-            address_validator.validate(address_with_mandatory_fields)
+            address_validator.validate(self.address_with_mandatory_fields)
         except DataDoesNotMatchSchemaException as e:
-            self.fail('Could not validate address: ' + repr(e))
+            self.fail("Could not validate address: " + repr(e))
 
-    def test_length_of_fields_is_checked(self):
-        try:
-            address_validator.validate({
-                u'line_one': u'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'  # over 40 chars
-            })
-            self.fail("Should have thrown exception")
-        except DataDoesNotMatchSchemaException as e:
-            self.assertEqual(e.field_errors['line_one'],
-                             'line_one is a required unicode field and must be a maximum of 40 characters long')
-
-    def test_can_detect_missing_fields_from_exception(self):
+    def test_can_detect_correct_number_of_missing_fields_from_exception(self):
         try:
             address_validator.validate({})
         except DataDoesNotMatchSchemaException as exception:
             print repr(exception.field_errors)
-            self.assertEquals(len(exception.field_errors), 4)  # we have four required fields
+            self.assertEquals(len(exception.field_errors), 8)  # we have eight required fields
