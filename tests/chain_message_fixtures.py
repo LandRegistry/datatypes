ec951cce1975902d015df21a162d6d8c28ce6f2e
Mat Wall <mat.wall@digital.cabinet-office.gov.uk>
2014-10-06 13:06:48 +0100

    Now we'll send one message per chain. Far simpler on everyone's brains.

--- tests/chain_message_fixtures.py
+++ tests/chain_message_fixtures.py
@@ -5,56 +5,31 @@ valid_chain_message = unicoded({
     'message_envelope': {
         'caused_by_blockchain_insert_id': 3954,
 
-        'messages': [
-            {
-                'message': {
-                    'object': {
-                        'reason_for_change': 'str',
-                        'db_id': 3953,
-                        'data': '<data>',
-                        'initial_request_timestamp': '123456',
-                        'created_by': 'The Mint',
-                        'creation_timestamp': 1,
-                        'object_id': 'AB12345',
-                        'chains': [
-                            {
-                                'chain_name': 'history',
-                                'chain_value': 'AB12345'
-                            },
-                            {
-                                'chain_name': 'sausage',
-                                'chain_value': 'walls'
-                            }
-                        ],
-                        'blockchain_index': 3953
-                    },
+        'message': {
+            'message': {
+                'object': {
+                    'reason_for_change': 'str',
+                    'db_id': 3953,
+                    'data': '<data>',
+                    'initial_request_timestamp': '123456',
+                    'created_by': 'The Mint',
+                    'creation_timestamp': 1,
+                    'object_id': 'AB12345',
+                    'chains': [
+                        {
+                            'chain_name': 'history',
+                            'chain_value': 'AB12345'
+                        },
+                        {
+                            'chain_name': 'sausage',
+                            'chain_value': 'walls'
+                        }
+                    ],
+                    'blockchain_index': 3953
                 },
-                'chain_name': 'sausage',
             },
-            {
-                'message': {
-                    'object': {
-                        'reason_for_change': 'str',
-                        'data': '<data>',
-                        'initial_request_timestamp': '123456',
-                        'created_by': 'The Mint',
-                        'creation_timestamp': 1,
-                        'object_id': 'AB12345',
-                        'chains': [
-                            {
-                                'chain_name': 'history',
-                                'chain_value': 'AB12345'
-                            },
-                            {
-                                'chain_name': 'sausage',
-                                'chain_value': 'walls'
-                            }
-                        ],
-                    },
-                },
-                'chain_name': 'history',
-            }
-        ]
+            'chain_name': 'sausage',
+        },
     }
 })
 
@@ -62,53 +37,28 @@ valid_chain_message = unicoded({
 another_valid_message = unicoded({
     'message_envelope': {
         'caused_by_blockchain_insert_id': 4188,
-        'messages': [
-            {
-                'message': {
-                    'object': {
-                        'reason_for_change': 'str',
-                        'data': '<data>',
-                        'initial_request_timestamp': '123456',
-                        'created_by': 'The Mint',
-                        'object_id': 'AB12345',
-                        'chains': [
-                            {
-                                'chain_name': 'history',
-                                'chain_value': 'AB12345'
-                            },
-                            {
-                                'chain_name': 'sausage',
-                                'chain_value': 'walls'
-                            }
-                        ]
-                    }
-                },
-                'chain_name': 'sausage'
+        'message': {
+            'message': {
+                'object': {
+                    'reason_for_change': 'str',
+                    'data': '<data>',
+                    'initial_request_timestamp': '123456',
+                    'created_by': 'The Mint',
+                    'object_id': 'AB12345',
+                    'chains': [
+                        {
+                            'chain_name': 'history',
+                            'chain_value': 'AB12345'
+                        },
+                        {
+                            'chain_name': 'sausage',
+                            'chain_value': 'walls'
+                        }
+                    ]
+                }
             },
-            {
-                'message': {
-                    'object': {
-                        'reason_for_change': 'str',
-                        'data': '<data>',
-                        'initial_request_timestamp': '123456',
-                        'created_by': 'The Mint',
-                        'creation_timestamp': 1,
-                        'object_id': 'AB12345',
-                        'chains': [
-                            {
-                                'chain_name': 'history',
-                                'chain_value': 'AB12345'
-                            },
-                            {
-                                'chain_name': 'sausage',
-                                'chain_value': 'walls'
-                            }
-                        ],
-                    }
-                },
-                'chain_name': 'history'
-            }
-        ]
+            'chain_name': 'sausage'
+        }
     }
 })
 
